47b47801f1350119155b2801bc8efabc0dee03eb
leandro <leandro_antunes28@hotmail.com>
2025-05-12 15:37:24 -0300

    criando estrutura atividades

---
+++ 01/index.js
@@ -0,0 +1,14 @@
+// Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
+// utilizando uma estrutura de controle if.
+
+
+const numero = 5;
+
+if (numero % 2 === 0) {
+    console.log(`${numero} é par`);
+}
+else {
+    console.log(`${numero} é ímpar`);
+}
+
+
